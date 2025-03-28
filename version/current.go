47d58973d4b8c90c2fd3392ac70716f681719705
Rafael Mori <faelmori@gmail.com>
2025-03-28 15:08:54 -0300

    chore: update version to v1.1.2 in CLI_VERSION and current.go
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- version/current.go
+++ version/current.go
@@ -37,7 +37,7 @@ var (
 )
 
 const gitModelUrl = "https://github.com/faelmori/xtui.git"
-const currentVersionFallback = "v1.1.0" // First version with the version file
+const currentVersionFallback = "v1.1.2" // First version with the version file
 
 //go:embed CLI_VERSION
 var currentVersion string
